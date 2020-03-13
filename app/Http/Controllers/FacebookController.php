f59fd54441ccc70feeac13cb77e1c35454960302
andreychernykh@me.com <andreychernykh@me.com>
2020-03-13 12:07:31 +0300

    fix

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Laravel\Socialite\Facades\Socialite;
 
 class FacebookController extends Controller
@@ -14,10 +15,23 @@ class FacebookController extends Controller
     }
 
     public function redirect() {
-        if (\Auth::user()) {
+        if (\Auth::user() || (!$this->request->has('code') || $this->request->has('denied'))) {
             return redirect()->route('index');
         }
+
         $user = Socialite::driver('facebook')->user();
-        dd($user);
+
+        $appUser = User::where('email', $user->email)->first();
+        if (!$appUser) {
+            $appUser = new User();
+            $appUser->email = $user->email;
+            $appUser->name = $user->name;
+            $appUser->ext_id = $user->id;
+            $appUser->password = bcrypt($user->token);
+            $appUser->save();
+        }
+
+        \Auth::login($appUser);
+        return redirect()->route('index');
     }
 }
